6b4cefbd029c065a5b30a77f75960b22fafbdb21
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-06-28 19:08:07 -0300

    Fix validations User:bedel

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   enum :role, { bedel: 0, admin: 1 }
   enum :turno, { todos: 0, mañana: 1, tarde: 2, noche: 3 }
-  validates :nombre, presence: true
-  validates :apellido, presence: true
-
+  validates :nombre, presence: true, if: :bedel?
+  validates :apellido, presence: true, if: :bedel?
+  validates :turno, presence: true, if: :bedel?
   def self.bedels
     User.where(role: :bedel)
   end
