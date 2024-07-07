6fe22c06ef8be0ead63bab3ba2208f4a3bcf265e
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-07-07 01:52:11 -0300

    style: Indent

--- db/migrate/20240603184531_create_usuarios.rb
+++ db/migrate/20240603184531_create_usuarios.rb
@@ -1,12 +1,12 @@
 class CreateUsuarios < ActiveRecord::Migration[7.1]
   def change
     create_table :usuarios, id: :string do |t|
-      t.integer :role , default: 0
+      t.integer :role, default: 0
       t.integer :turno
       t.string :nombre
       t.string :apellido
       t.string :password_digest
-      t.timestamps
+      t.datetime :deleted_at
     end
   end
 end
