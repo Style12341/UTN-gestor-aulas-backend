5c9d344a454ad04441e96b1957baba0a666eb5ce
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-08-18 23:33:32 -0300

    refactor: Add regular as option instead of sin_recursos

--- app/models/aula.rb
+++ app/models/aula.rb
@@ -1,6 +1,6 @@
 class Aula < ApplicationRecord
   validates_presence_of Aula.attribute_names - %w[id]
-  enum tipo: { sin_recursos: 0, informatica: 1, multimedia: 2 }
+  enum tipo: { regular: 0, sin_recursos: 0, informatica: 1, multimedia: 2 }
   enum tipo_pizarron: { tiza: 0, marcador: 1 }
   has_many :periodos_reservados, class_name: 'RenglonReservaPeriodica', dependent: :destroy, foreign_key: 'aula_id'
   has_many :dias_reservados, class_name: 'RenglonReservaEsporadica', dependent: :destroy, foreign_key: 'aula_id'
