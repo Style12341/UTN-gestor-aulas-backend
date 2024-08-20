1937ed0bceda176018eecc98524555f87c07a9df
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-08-20 02:02:49 -0300

    refactor: Refactor deprecated enums definition

--- app/models/aula.rb
+++ app/models/aula.rb
@@ -1,7 +1,7 @@
 class Aula < ApplicationRecord
   validates_presence_of Aula.attribute_names - %w[id]
-  enum tipo: { regular: 0, sin_recursos: 0, informatica: 1, multimedia: 2 }
-  enum tipo_pizarron: { tiza: 0, marcador: 1 }
+  enum :tipo, regular: 0, sin_recursos: 0, informatica: 1, multimedia: 2
+  enum :tipo_pizarron, tiza: 0, marcador: 1
   has_many :periodos_reservados, class_name: 'RenglonReservaPeriodica', dependent: :destroy, foreign_key: 'aula_id'
   has_many :dias_reservados, class_name: 'RenglonReservaEsporadica', dependent: :destroy, foreign_key: 'aula_id'
   has_many :caracteristicas_aula, dependent: :destroy
--- app/models/renglon_reserva_periodica.rb
+++ app/models/renglon_reserva_periodica.rb
@@ -1,6 +1,6 @@
 class RenglonReservaPeriodica < ApplicationRecord
   validates_presence_of RenglonReservaPeriodica.attribute_names - %w[id]
-  enum dia: { lunes: 1, martes: 2, miercoles: 3, jueves: 4, viernes: 5, sabado: 6, domingo: 0 }
+  enum :dia, lunes: 1, martes: 2, miercoles: 3, jueves: 4, viernes: 5, sabado: 6, domingo: 0
   belongs_to :reserva, class_name: 'ReservaPeriodica', foreign_key: 'reserva_id'
   belongs_to :aula, class_name: 'Aula', foreign_key: 'aula_id'
   scope :with_least_overlap, lambda { |range_or_from:, to: nil|
